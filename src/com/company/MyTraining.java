7777bee18dcceccd326821b2aa85945bc42c7668
Paul25 <missile2013@yandex.ru>
2015-04-05 12:28:29 +0300

    05.04.java

--- src/com/company/MyTraining.java
+++ src/com/company/MyTraining.java
@@ -14,10 +14,10 @@ import java.util.List;
  */
 public class MyTraining {
 
-    public static void main(String[] args)
+    //public static void main(String[] args)
 
-    {
-        //1
+
+     //1
         /*
         System.setProperty("webdriver.chrome.driver","E:/Course/chromedriver_win32/chromedriver.exe");
         WebDriver h=new ChromeDriver();
@@ -49,15 +49,84 @@ public class MyTraining {
         System.out.println(f.get(2));
         */
         //3
+        public static void print (List<String> s)
+        {
+
+        for (int i = 0; i < s.size(); i++)
+        {
+
+            System.out.println(s.get(i));
+        }
+
+        }
+        public static boolean areListsEqual (List s, List f){
+
+        if (s.equals(f)){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void print (String[] a){
+
+        for (int i = 0; i < a.length; i++) {
+
+            System.out.print(a[i]);
+
+        }
+
+        System.out.println();
+
+    }
+    public static boolean isEqual(String[]a, List<String> s){
+
+        if (a.length == s.size()) {
+
+            /*for (int i = 0; i < a.length; i++) {
+
+                if (a[i] == s.get(i)){
+                    System.out.println(a[i] + " = " + s.get(i));
+
+                }
+           */  return true;}
+
+        return false;
+    }
+
+
+    public static void main(String[] args)  {
+
         List<String> s = new ArrayList<String>();
         s.add("a");
         s.add("b");
         s.add("c");
         s.add("d");
+        print(s);
 
-                     System.out.print(s);      
+        List<String> f = new ArrayList<String>();
+        f.add("a");
+        f.add("b");
+        f.add("c");
+        f.add("d");
+        System.out.println(areListsEqual(s, f));
 
+        f.add("d");
+        System.out.println(areListsEqual(s, f));
 
+        s.add("b");
+        System.out.println(areListsEqual(s, f));
+
+        List<String> s1 = new ArrayList<String>();
+        List<String> s2 = new ArrayList<String>();
+        System.out.println(areListsEqual(s1, s2));
+
+        String[] a = {"a", "b", "c", "d", "b"};
+        print(a);
+
+        System.out.println(isEqual(a, s));
 
     }
+
 }
+
