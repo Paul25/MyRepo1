de84205197fba515ba25932a3dd0cbd988959fde
Paul25 <missile2013@yandex.ru>
2015-04-15 21:12:03 +0300

    L8.java

--- src/com/company/Shortcom.java
+++ src/com/company/Shortcom.java
@@ -11,10 +11,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 @RunWith(JUnit4.class)
-public class AngelTest
+public class Shortcom
 {
     public WebDriver dr;
+    public void mas(String master)
+    {
+        dr.findElement(By.name("master")).sendKeys(master);
+    }
+    public void mysite (String site)
+    {
+        dr.findElement(By.name("site")).sendKeys(site);
+            }
+    public void sentkey ()
+    {
+        dr.findElement(By.name("site")).sendKeys(Keys.ENTER);
+            }
+    /*public void sentGener ()
+    {
 
+        dr.findElement(By.name("site")).sendKeys(Keys."Generate");
+        }
+        */
+    public String passwrd()
+    {
+        String ps=dr.findElement(By.name("password")).getAttribute("value");
+        return ps;
+    }
 
     @After
     public void clean()
@@ -22,17 +44,22 @@ public class AngelTest
         dr.quit();
     }
 
-   @Test
-   public void test3() throws InterruptedException {
+    @Test
+    public void test3() throws InterruptedException {
+        mas("12345678");
+        mysite ("missile2013@yandex.ru");
 
-       dr.findElement(By.name("master")).sendKeys("12345678");
-       dr.findElement(By.name("site")).sendKeys("missile2013@yandex.ru");
-       dr.findElement(By.name("site")).sendKeys(Keys.ENTER);
-       dr.findElement(By.name("password")).getAttribute("value");
+
+        /*
+        dr.findElement(By.name("master")).sendKeys("12345678");
+        dr.findElement(By.name("site")).sendKeys("missile2013@yandex.ru");
+        dr.findElement(By.name("site")).sendKeys(Keys.ENTER);
+        dr.findElement(By.name("password")).getAttribute("value");
         String s=dr.findElement(By.name("password")).getAttribute("value");
-        System.out.println(s);
+        */
+        System.out.println(passwrd());
         Thread.sleep(5000);
-        Assert.assertEquals("nVQiy+y71a",s);
+        Assert.assertEquals("nVQiy+y71a",passwrd());
 
     }
 
@@ -40,24 +67,31 @@ public class AngelTest
 
     @Test
     public void test4() throws InterruptedException {
-
+        mas("");
+        mysite ("missile2013@yandex.ru");
+      /*
         dr.findElement(By.name("master")).sendKeys("");
         dr.findElement(By.name("site")).sendKeys("missile2013@yandex.ru");
         dr.findElement(By.name("password")).getAttribute("value");
         String s=dr.findElement(By.name("password")).getAttribute("value");
-        System.out.println(s);
+       */
+        System.out.println(passwrd());
         Assert.assertEquals("Password generator",dr.getTitle());
         //Thread.sleep(5000);
         //dr.quit();
     }
     @Test
     public void test5() throws InterruptedException {
+        mas("12345678");
+        mysite ("");
 
+       /*
         dr.findElement(By.name("master")).sendKeys("12345678");
         dr.findElement(By.name("site")).sendKeys("");
         dr.findElement(By.name("password")).getAttribute("value");
         String s=dr.findElement(By.name("password")).getAttribute("value");
-        System.out.println(s);
+       */
+        System.out.println(passwrd());
         Assert.assertEquals("Password generator",dr.getTitle());
         //Thread.sleep(5000);
         //h.quit();
