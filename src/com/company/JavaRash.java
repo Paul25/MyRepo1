da9b5ec2b13d945cd25927545f280f90492a5110
Paul25 <missile2013@yandex.ru>
2015-04-22 20:49:46 +0300

    L10.java

--- src/com/company/JavaRash.java
+++ src/com/company/JavaRash.java
@@ -22,6 +22,26 @@ public class JavaRash {
         else m2 = b;
         return m2; //Напишите тут ваш код
     }
+   public static void printN(String s1) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+
+
+                System.out.print(s1 + " ");
+            }
+            System.out.println();
+        }
+   }
+/*
+
+        int j = 0;
+        for (int i = 0; i < 5; i++)
+            for (j; j < 10; j++)
+                System.out.print(s1 + " " ); // но в конце у тебя будет пробел
+        j = 0;
+*/
+
+
 
     public static void main(String[] args) throws Exception
     {
@@ -53,5 +73,81 @@ public class JavaRash {
         {
             System.out.println("7) Неправильно для равных двух пар"); //вывод=5
         }
+
+
+        { String s="MAY 6 1983";
+            System.out.println("I was born "+s);
+        }
+
+        printN("Pasha");
+        //Вывести на экран произведение 10 чисел от 1 до 10. Результат - 1 число.
+        //Подсказка: будет три миллиона с хвостиком.
+        int mp=1;
+        for(int i1=1; i1<=10; i1++)
+        {
+         mp=mp*i1;
+        }System.out.println(mp);
+        /*Сумма 10 чисел
+        Вывести на экран сумму чисел от 1 до 10 построчно:
+        1
+        1+2=3
+        1+2+3=6
+        1+2+3+4=10
+        */
+
+        int mp1=1;
+        for(int i2=2; i2<=10; i2++)
+        {
+            System.out.println(mp1);
+            mp1=mp1+i2;
+
+        }
+        /* Мама мыла раму
+Вывести на экран все возможные комбинации слов «Мама», «Мыла», «Раму».
+Подсказка: их 6 штук. Каждую комбинацию вывести с новой строки. Слова не разделять. Пример:
+МылаРамуМама
+РамуМамаМыла
+...
+*/
+        String s="Мама";
+        String s1="Мыла";
+        String s2="Раму";
+        String[] text ={"Мама", "Мыла", "Раму"};
+        int n = text.length;
+        for (int i1 = 0; i1 < n; i1++) {
+            for (int i2 = 0; i2 < n; i2++) {
+                for (int i3 = 0; i3 < n; i3++) {
+                    if (i1!=i2&i1!=i3&i2!=i3)
+                        System.out.println(text[i1]+text[i2]+text[i3]);
+                }
+            }
+        }
+        /* Таблица умножения
+Выведи на экран таблицу умножения 10 на 10 в следующем виде:
+1 2 3 …
+2 4 6 …
+3 6 9 …
+…
+*/
+        int f=1;
+        int r=1;
+        for(int i1=1; i1<=10; i1++)
+        {
+             r=i1;
+            for (int j1 = 1; j1 <= 10; j1++)
+            {
+               f = j1;
+              System.out.print((r*f)+" ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
+
+
+
+
+
+
--- src/com/company/PageObject.java
+++ src/com/company/PageObject.java
@@ -15,54 +15,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(JUnit4.class)
-public class Xpath
-{
+public class PageObject {
     public WebDriver dr;
-    public void mas(String master)
-    {
-        dr.findElement(By.xpath("//td[text()='Your master password']/following::input[1]")).sendKeys(master);
+
+    public void mas(String master) {
+        dr.findElement(By.name("master")).sendKeys(master);
     }
-    public void mysite (String site)
-    {
-        dr.findElement(By.xpath("//td[text()='Site name']/following::input[1]")).sendKeys(site);
+
+    public void mysite(String site) {
+        dr.findElement(By.name("site")).sendKeys(site);
     }
-    /*public void sentkey ()
-    {
-         dr.findElement(By.xpath("//td[text()='Site name']/following::input[1]").sendKeys(Keys.ENTER));
-    }*/
+
+    public void sentkey() {
+        dr.findElement(By.name("site")).sendKeys(Keys.ENTER);
+    }
+
     /*
     dr.findElement(By.name("site")).sendKeys(Keys."Generate");
-    WebElement pasha= dr.findElement(By.xpath("//input[@type='submit']"));//td[text()='Site name']/following::input[1]
     */
-    public String passwrd() {String ps=dr.findElement(By.name("password")).getAttribute("value");
+    public String passwrd() {
+        String ps = dr.findElement(By.name("password")).getAttribute("value");
         return ps;
     }
-    public void generate(){
+
+    public void generate() {
         List<WebElement> list = new ArrayList<WebElement>(dr.findElements(By.tagName("input")));
         list.get(2).click();
     }
 
     @Before
-    public void qwerty()
-    {
+    public void qwerty() {
 
         System.setProperty("webdriver.chrome.driver", "E:/Course/chromedriver_win32/chromedriver.exe");
         dr = new ChromeDriver();
         dr.manage().window().maximize();
-        dr.get("http://angel.net/~nic/passwd.sha1.1a.html");
-        //dr.get("http://oxogamestudio.com/passwd.current6.htm");
+        //dr.get("http://angel.net/~nic/passwd.sha1.1a.html");
+        dr.get("http://oxogamestudio.com/passwd.current6.htm");
     }
 
     @After
-    public void clean()
-    {
+    public void clean() {
         dr.quit();
     }
 
     @Test
     public void test3() throws InterruptedException {
         mas("12345678");
-        mysite ("missile2013@yandex.ru");
+        mysite("missile2013@yandex.ru");
         generate();
         /*
         dr.findElement(By.name("master")).sendKeys("12345678");
@@ -73,36 +72,38 @@ public class Xpath
         */
         System.out.println(passwrd());
         Thread.sleep(5000);
-        Assert.assertEquals("nVQiy+y71a",passwrd());
+        Assert.assertEquals("nVQiy+y71a", passwrd());
     }
 
     @Test
     public void test4() throws InterruptedException {
         mas("");
-        mysite ("missile2013@yandex.ru");
+        mysite("missile2013@yandex.ru");
         generate();
         System.out.println(passwrd());
-        Assert.assertEquals("Password generator",dr.getTitle());
-         }
-    @Test
-    public void test5() throws InterruptedException {
-        mas("12345678");
-        mysite ("");
-        generate();
-        System.out.println(passwrd());
-        Assert.assertEquals("Password generator",dr.getTitle());
+        Assert.assertEquals("Password generator", dr.getTitle());
     }
 
     @Test
-    public void test6(){
+    public void test5() throws InterruptedException {
+        mas("12345678");
+        mysite("");
+        generate();
+        System.out.println(passwrd());
+        Assert.assertEquals("Password generator", dr.getTitle());
+    }
+
+    @Test
+    public void test6() {
         List<WebElement> list = new ArrayList<WebElement>();
         list = dr.findElements(By.tagName("input"));
         list.get(2).click();
         System.out.println(passwrd());
-        Assert.assertEquals("Test failed!!!","BaefBs8/Z/cm2@1a", passwrd());
+        Assert.assertEquals("Test failed!!!", "BaefBs8/Z/cm2@1a", passwrd());
     }
+
     @Test
-    public void test7(){
+    public void test7() {
         String str = "";
         for (int i = 0; i < 200; i++)
             str = str + 1;
@@ -110,22 +111,24 @@ public class Xpath
         mysite(str);
         generate();
         System.out.println(passwrd());
-        Assert.assertEquals("Test failed!!!","aR8ztwNBbSqe5@1a", passwrd());
+        Assert.assertEquals("Test failed!!!", "aR8ztwNBbSqe5@1a", passwrd());
     }
+
     @Test
-    public void test8(){
+    public void test8() {
 
         mas("/';*&#&$@^!_...\\n");
         mysite("/';*&#&$@^!_...\\n");
         generate();
         System.out.println(passwrd());
-        Assert.assertEquals("Test failed!!!","ctolW6AdI0te1@1a", passwrd());
+        Assert.assertEquals("Test failed!!!", "ctolW6AdI0te1@1a", passwrd());
     }
+
     @Test
-    public void test9(){
+    public void test9() {
 //        List<WebElement> list = new ArrayList<WebElement>(dr.findElements(By.tagName("input")));
         //input[@type='submit']
-        WebElement pasha= dr.findElement(By.xpath("//input[@type='submit']"));
+        WebElement pasha = dr.findElement(By.xpath("//input[@type='submit']"));
 //        String title = list.get(2).getAttribute("value");
         //String title = pasha.getAttribute("value");
 
@@ -134,53 +137,60 @@ public class Xpath
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Assert.assertEquals("Test failed!!!","Generate", pasha.getAttribute("value"));
+        Assert.assertEquals("Test failed!!!", "Generate", pasha.getAttribute("value"));
     }
+
     @Test
-    public void test10(){
+    public void test10() {
         mas("/';*&#&$@^!_...\\n");
         mysite("/';*&#&$@^!_...\\n");
         generate();
         Assert.assertEquals("/';*&#&$@^!_...\\n", dr.findElement(By.name("master")).getAttribute("value"));
     }
+
     @Test
-    public void test11(){
+    public void test11() {
         mas("/';*&#&$@^!_...\\n");
         mysite("/';*&#&$@^!_...\\n");
         generate();
-        Assert.assertEquals("Master is not editable",true, dr.findElement(By.name("master")).isEnabled());
+        Assert.assertEquals("Master is not editable", true, dr.findElement(By.name("master")).isEnabled());
     }
+
     @Test
-    public void test12(){
+    public void test12() {
         mas("12345678");
         mysite("gmail.com");
         generate();
-        Assert.assertEquals("Site is not editable",true, dr.findElement(By.name("site")).isEnabled());
+        Assert.assertEquals("Site is not editable", true, dr.findElement(By.name("site")).isEnabled());
     }
+
     @Test
-    public void test13(){
+    public void test13() {
         mas("12345678");
         mysite("gmail.com");
         generate();
-        Assert.assertEquals("Password is not editable",true, dr.findElement(By.name("password")).isEnabled());
+        Assert.assertEquals("Password is not editable", true, dr.findElement(By.name("password")).isEnabled());
     }
+
     @Test
-    public void test14(){
+    public void test14() {
         List<WebElement> list = new ArrayList<WebElement>(dr.findElements(By.tagName("td")));
         String title = list.get(0).getText();
-        Assert.assertEquals("Test failed!!!","Your master password", title);
+        Assert.assertEquals("Test failed!!!", "Your master password", title);
     }
+
     @Test
-    public void test15(){
+    public void test15() {
         List<WebElement> list = new ArrayList<WebElement>(dr.findElements(By.tagName("td")));
         String title = list.get(2).getText();
-        Assert.assertEquals("Test failed!!!","Site name", title);
+        Assert.assertEquals("Test failed!!!", "Site name", title);
     }
+
     @Test
-    public void test16(){
+    public void test16() {
         List<WebElement> list = new ArrayList<WebElement>(dr.findElements(By.tagName("td")));
         String title = list.get(5).getText();
-        Assert.assertEquals("Test failed!!!","Generated password", title);
+        Assert.assertEquals("Test failed!!!", "Generated password", title);
     }
 
 
