a4ff7eecf2b46a516f5d7916ae35cb58d809a7dd
Paul25 <missile2013@yandex.ru>
2015-11-19 20:33:46 +0200

    19.11 MaxChanges

---
+++ src/main/java/com/company/Starter1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by Marchenko on 19.11.2015.
+ */
+public class Starter1 {
+    public static void main(String[] args) throws Exception {
+
+
+        int w1 = 80;
+        int e2 = 100;
+        int r = w1 + e2;
+
+        System.out.print(r);
+}}
